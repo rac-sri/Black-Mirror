1d93ea783ea022ffd05954636d02d73628d22712
rachit2501 <rac.sri25@gmail.com>
2020-07-12 07:17:51 +0530

    more screens

---
+++ BlackMirrorWeb/src/components/Scenario/Scenario.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import "./scenario.css";
+
+export default function Scenario() {
+    return (
+        <div id="PLAYER__1">
+            <div id="CREATE_SCENARIO">
+                <span>
+                    CREATE
+                    <br />
+                    SCENARIO
+                </span>
+            </div>
+            <div id="OPTA">
+                <span>OPTA:</span>
+            </div>
+            <div id="OPTB">
+                <span>OPTB:</span>
+            </div>
+            <svg className="Rectangle_1673">
+                <rect
+                    id="Rectangle_1673"
+                    rx={5}
+                    ry={5}
+                    x={0}
+                    y={0}
+                    width={373}
+                    height={41}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1674">
+                <rect
+                    id="Rectangle_1674"
+                    rx={5}
+                    ry={5}
+                    x={0}
+                    y={0}
+                    width={373}
+                    height={42}
+                ></rect>
+            </svg>
+            <div id="ENVIRONMENT">
+                <span>ENVIRONMENT:</span>
+            </div>
+            <svg className="Rectangle_1672">
+                <rect
+                    id="Rectangle_1672"
+                    rx={5}
+                    ry={5}
+                    x={0}
+                    y={0}
+                    width={373}
+                    height={42}
+                ></rect>
+            </svg>
+            <div id="NAME">
+                <span>NAME:</span>
+            </div>
+            <svg className="Rectangle_1671">
+                <rect
+                    id="Rectangle_1671"
+                    rx={5}
+                    ry={5}
+                    x={0}
+                    y={0}
+                    width={373}
+                    height={41}
+                ></rect>
+            </svg>
+            <div id="DESCRIPTION">
+                <span>DESCRIPTION:</span>
+            </div>
+            <svg className="Rectangle_1670">
+                <rect
+                    id="Rectangle_1670"
+                    rx={20}
+                    ry={20}
+                    x={0}
+                    y={0}
+                    width={690}
+                    height={447}
+                ></rect>
+            </svg>
+            <div id="OPTC">
+                <span>OPTC:</span>
+            </div>
+            <svg className="Rectangle_1675">
+                <rect
+                    id="Rectangle_1675"
+                    rx={5}
+                    ry={5}
+                    x={0}
+                    y={0}
+                    width={373}
+                    height={42}
+                ></rect>
+            </svg>
+            <svg className="Path_2_b" viewBox="2 9.33 52.787 45.293">
+                <linearGradient
+                    id="Path_2_b"
+                    spreadMethod="pad"
+                    x1="0.5"
+                    x2="0.5"
+                    y1={0}
+                    y2={1}
+                >
+                    <stop offset={0} stopColor="#cef6ff" stopOpacity={1} />
+                    <stop offset={1} stopColor="#657dff" stopOpacity={1} />
+                </linearGradient>
+                <path
+                    id="Path_2_b"
+                    d="M 29.07244110107422 9.329999923706055 L 31.17298698425293 9.329999923706055 L 31.17298698425293 54.62303924560547 L 29.07244110107422 54.62303924560547 L 29.07244110107422 9.329999923706055 Z M 26.66791725158691 9.659194946289062 L 26.73725700378418 42.24950790405273 L 6.191283702850342 42.24950790405273 L 6.191283702850342 36.83389663696289 L 6.191283702850342 27.75021743774414 L 6.191283702850342 21.70423126220703 L 26.66791725158691 9.659194946289062 Z M 7.592113494873047 23.10506248474121 L 7.592113494873047 29.15104866027832 L 2 29.15104866027832 L 2 35.43306732177734 L 7.592113494873047 35.43306732177734 L 7.592113494873047 40.84867477416992 L 25.33642959594727 40.84867477416992 L 25.33642959594727 23.10506248474121 L 7.592113494873047 23.10506248474121 Z M 53.36983489990234 41.54838943481445 C 53.36703491210938 39.53189468383789 53.36423110961914 37.51609802246094 53.36143112182617 35.49960708618164 L 54.78747177124023 35.43657302856445 L 54.77066421508789 29.14544105529785 L 53.35022735595703 29.14544105529785 L 53.36983489990234 22.4046459197998 L 34.22469329833984 22.4046459197998 C 34.2401008605957 33.08807754516602 34.25481033325195 43.77149963378906 34.27021789550781 54.45493698120117 L 53.36983489990234 41.54838943481445 Z"
+                ></path>
+            </svg>
+        </div>
+    );
+}
---
+++ BlackMirrorWeb/src/components/yourRole/YourRole.tsx
@@ -0,0 +1,246 @@
+import React from "react";
+import "./yourrole.css";
+
+export default function YourRole() {
+    return (
+        <div id="PLAYER">
+            <svg className="Rectangle_492">
+                <rect
+                    id="Rectangle_492"
+                    rx={40}
+                    ry={40}
+                    x={0}
+                    y={0}
+                    width={700}
+                    height={850}
+                ></rect>
+            </svg>
+            <svg className="Path_2_bs" viewBox="2 9.33 52.787 45.293">
+                <linearGradient
+                    id="Path_2_bs"
+                    spreadMethod="pad"
+                    x1="0.5"
+                    x2="0.5"
+                    y1={0}
+                    y2={1}
+                >
+                    <stop offset={0} stopColor="#cef6ff" stopOpacity={1} />
+                    <stop offset={1} stopColor="#657dff" stopOpacity={1} />
+                </linearGradient>
+                <path
+                    id="Path_2_bs"
+                    d="M 29.07244110107422 9.329999923706055 L 31.17298698425293 9.329999923706055 L 31.17298698425293 54.62303924560547 L 29.07244110107422 54.62303924560547 L 29.07244110107422 9.329999923706055 Z M 26.66791725158691 9.659194946289062 L 26.73725700378418 42.24950790405273 L 6.191283702850342 42.24950790405273 L 6.191283702850342 36.83389663696289 L 6.191283702850342 27.75021743774414 L 6.191283702850342 21.70423126220703 L 26.66791725158691 9.659194946289062 Z M 7.592113494873047 23.10506248474121 L 7.592113494873047 29.15104866027832 L 2 29.15104866027832 L 2 35.43306732177734 L 7.592113494873047 35.43306732177734 L 7.592113494873047 40.84867477416992 L 25.33642959594727 40.84867477416992 L 25.33642959594727 23.10506248474121 L 7.592113494873047 23.10506248474121 Z M 53.36983489990234 41.54838943481445 C 53.36703491210938 39.53189468383789 53.36423110961914 37.51609802246094 53.36143112182617 35.49960708618164 L 54.78747177124023 35.43657302856445 L 54.77066421508789 29.14544105529785 L 53.35022735595703 29.14544105529785 L 53.36983489990234 22.4046459197998 L 34.22469329833984 22.4046459197998 C 34.2401008605957 33.08807754516602 34.25481033325195 43.77149963378906 34.27021789550781 54.45493698120117 L 53.36983489990234 41.54838943481445 Z"
+                ></path>
+            </svg>
+            <div id="profile">
+                <svg className="Path_1166" viewBox="0 0 173.815 173.815">
+                    <path
+                        id="Path_1166"
+                        d="M 0 0 L 173.81494140625 0 L 173.81494140625 173.81494140625 L 0 173.81494140625 L 0 0 Z"
+                    ></path>
+                </svg>
+                <svg className="Path_1167" viewBox="96 102.662 108.634 104.108">
+                    <path
+                        id="Path_1167"
+                        d="M 204.6343383789062 206.7694396972656 L 96 206.7694396972656 L 96 191.6903076171875 C 96.00131988525391 188.027587890625 98.20928192138672 184.7256164550781 101.5948181152344 183.3265686035156 L 132.4360046386719 170.5827026367188 L 132.2463684082031 162.9297790527344 L 123.1585845947266 149.2855224609375 L 123.1585845947266 130.7691345214844 C 122.9583435058594 116.891471862793 133.0247802734375 104.9936904907227 146.7433166503906 102.8931427001953 C 154.4996948242188 101.8746948242188 162.321044921875 104.2457656860352 168.2049560546875 109.4029846191406 C 174.0888977050781 114.5588684082031 177.4677886962891 121.999626159668 177.4757537841797 129.8236389160156 L 177.4757537841797 149.2855224609375 L 168.3879547119141 162.919189453125 L 168.1983337402344 170.5721130371094 L 199.0395050048828 183.3146362304688 C 202.4290313720703 184.7163391113281 204.6369781494141 188.0236511230469 204.6343383789062 191.6903076171875 L 204.6343383789062 206.7694396972656 Z M 204.6343383789062 206.7694396972656"
+                    ></path>
+                </svg>
+                <div id="Group_85">
+                    <svg className="Path_1168" viewBox="0 0 54.317 54.317">
+                        <path
+                            id="Path_1168"
+                            d="M 10.86343383789062 54.31716918945312 L 0 54.31716918945312 L 0 0 L 54.31716918945312 0 L 54.31716918945312 10.86343383789062 L 10.86343383789062 10.86343383789062 L 10.86343383789062 54.31716918945312 Z M 10.86343383789062 54.31716918945312"
+                        ></path>
+                    </svg>
+                    <svg className="Path_1169" viewBox="0 368 48.885 48.885">
+                        <path
+                            id="Path_1169"
+                            d="M 48.88545227050781 416.8854370117188 L 0 416.8854370117188 L 0 368 L 10.86343383789062 368 L 10.86343383789062 406.0220031738281 L 48.88545227050781 406.0220031738281 L 48.88545227050781 416.8854370117188 Z M 48.88545227050781 416.8854370117188"
+                        ></path>
+                    </svg>
+                    <svg className="Path_1170" viewBox="352 0 54.317 54.317">
+                        <path
+                            id="Path_1170"
+                            d="M 406.3171691894531 54.31716918945312 L 395.4537353515625 54.31716918945312 L 395.4537353515625 10.86343383789062 L 352 10.86343383789062 L 352 0 L 406.3171691894531 0 L 406.3171691894531 54.31716918945312 Z M 406.3171691894531 54.31716918945312"
+                        ></path>
+                    </svg>
+                    <svg className="Path_1171" viewBox="352 352 54.317 54.317">
+                        <path
+                            id="Path_1171"
+                            d="M 406.3171691894531 406.3171691894531 L 352 406.3171691894531 L 352 395.4537353515625 L 395.4537353515625 395.4537353515625 L 395.4537353515625 352 L 406.3171691894531 352 L 406.3171691894531 406.3171691894531 Z M 406.3171691894531 406.3171691894531"
+                        ></path>
+                    </svg>
+                </div>
+                <svg className="Path_1172" viewBox="416 64 10.863 10.863">
+                    <path
+                        id="Path_1172"
+                        d="M 416 64 L 426.8634338378906 64 L 426.8634338378906 74.86343383789062 L 416 74.86343383789062 L 416 64 Z M 416 64"
+                    ></path>
+                </svg>
+                <svg className="Path_1173" viewBox="416 128 10.863 10.863">
+                    <path
+                        id="Path_1173"
+                        d="M 416 128 L 426.8634338378906 128 L 426.8634338378906 138.8634338378906 L 416 138.8634338378906 L 416 128 Z M 416 128"
+                    ></path>
+                </svg>
+            </div>
+            <div id="Name_Abc">
+                <span>Name Abc</span>
+            </div>
+            <div id="Lorem_ipsum_dolor_sit_amet_con">
+                <span>
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.”
+                    <br />
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.”
+                </span>
+            </div>
+            <div id="GENDER">
+                <span>GENDER:</span>
+            </div>
+            <div id="OCCUPATION">
+                <span>OCCUPATION:</span>
+            </div>
+            <div id="Abc">
+                <span>Abc</span>
+            </div>
+            <div id="Abc_b">
+                <span>Abc</span>
+            </div>
+            <div id="SALARY">
+                <span>SALARY:</span>
+            </div>
+            <div id="Abc_ca">
+                <span>Abc</span>
+            </div>
+            <div id="TRAIT1">
+                <span>TRAIT#1</span>
+            </div>
+            <div id="TRAIT1_cc">
+                <span>TRAIT#1</span>
+            </div>
+            <div id="TRAIT1_cd">
+                <span>TRAIT#1</span>
+            </div>
+            <div id="TRAIT1_ce">
+                <span>TRAIT#1</span>
+            </div>
+            <svg className="Rectangle_1632">
+                <rect
+                    id="Rectangle_1632"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={130}
+                    height={10}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1633">
+                <rect
+                    id="Rectangle_1633"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={130}
+                    height={10}
+                ></rect>
+            </svg>
+            <div id="TRAIT1_ch">
+                <span>TRAIT#1</span>
+            </div>
+            <div id="TRAIT1_ci">
+                <span>TRAIT#1</span>
+            </div>
+            <svg className="Rectangle_1634">
+                <rect
+                    id="Rectangle_1634"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={130}
+                    height={10}
+                ></rect>
+            </svg>
+            <div id="TRAIT1_ck">
+                <span>TRAIT#1</span>
+            </div>
+            <div id="TRAIT1_cl">
+                <span>TRAIT#1</span>
+            </div>
+            <svg className="Rectangle_1635">
+                <rect
+                    id="Rectangle_1635"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={130}
+                    height={10}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1636">
+                <rect
+                    id="Rectangle_1636"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={10}
+                    height={10}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1637">
+                <rect
+                    id="Rectangle_1637"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={10}
+                    height={10}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1638">
+                <rect
+                    id="Rectangle_1638"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={10}
+                    height={10}
+                ></rect>
+            </svg>
+            <svg className="Rectangle_1639">
+                <rect
+                    id="Rectangle_1639"
+                    rx={0}
+                    ry={0}
+                    x={0}
+                    y={0}
+                    width={10}
+                    height={10}
+                ></rect>
+            </svg>
+            <div id="YOUR_ROLE">
+                <span>YOUR ROLE</span>
+            </div>
+        </div>
+    );
+}
